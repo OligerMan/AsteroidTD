59aeed3aa50503a2e02a5f423e334571e02d10a8
OligerMan <32018536+OligerMan@users.noreply.github.com>
2019-10-10 01:42:25 +0300

    v0.1.1.12 - added research list

--- JustTest/Research.h
+++ JustTest/Research.h
@@ -10,10 +10,73 @@
 #include <iostream>
 
 enum ResearchList {
-	test_research1,
-	test_research2,
-	test_research3,
-	test_research4,
+	turret_initial,
+	turret_damage_lvl1,
+	turret_damage_lvl2,
+	turret_damage_lvl3,
+	turret_attack_speed_lvl1,
+	turret_attack_speed_lvl2,
+	turret_max_health_lvl1,
+	turret_max_health_lvl2,
+	turret_max_health_lvl3,
+	turret_regen_lvl1,
+	turret_regen_lvl2,
+	turret_damage_reduction_lvl1,
+	turret_damage_reduction_lvl2,
+	turret_damage_reduction_lvl3,
+	turret_damage_reduction_lvl4,
+	turret_lifesteal_lvl1,
+	turret_lifesteal_lvl2,
+	turret_lifesteal_lvl3,
+	turret_range_lvl1,
+	turret_range_lvl2,
+	turret_range_lvl3,
+	turret_range_lvl4,
+	turret_ultimate,
+
+	dome_initial,
+	dome_global_regen_lvl1,
+	dome_global_regen_lvl2,
+	dome_global_regen_lvl3,
+	dome_global_health_lvl1,
+	dome_global_health_lvl2,
+	dome_global_health_lvl3,
+	dome_max_health_lvl1,
+	dome_max_health_lvl2,
+	dome_max_health_lvl3,
+	dome_global_damage_reduction_lvl1,
+	dome_global_damage_reduction_lvl2,
+	dome_global_damage_reduction_lvl3,
+	dome_global_damage_lvl1,
+	dome_global_damage_lvl2,
+	dome_global_damage_lvl3,
+	dome_global_research_income_lvl1,
+	dome_global_research_income_lvl2,
+	dome_global_research_income_lvl3,
+	dome_global_gold_income_lvl1,
+	dome_global_gold_income_lvl2,
+	dome_global_gold_income_lvl3,
+	dome_ultimate,
+
+	research_initial,
+	research_income_lvl1,
+	research_income_lvl2,
+	research_income_lvl3,
+	research_max_health_lvl1,
+	research_max_health_lvl2,
+	research_max_health_lvl3,
+	research_regen_lvl1,
+	research_regen_lvl2,
+
+	gold_initial,
+	gold_income_lvl1,
+	gold_income_lvl2,
+	gold_income_lvl3,
+	gold_max_health_lvl1,
+	gold_max_health_lvl2,
+	gold_max_health_lvl3,
+	gold_regen_lvl1,
+	gold_regen_lvl2,
 
 	RESEARCH_COUNT
 };
@@ -38,10 +101,73 @@ class ResearchManager {
 	Point left_up_graph_corner;
 
     std::vector<std::string> research_name = {
-        "tr1",
-		"tr2",
-		"tr3",
-		"tr4"
+		"turret_init",
+		"turret_dmg1",    // dmg = damage
+		"turret_dmg2",
+		"turret_dmg3",
+		"turret_asp1",    // asp = attack speed
+		"turret_asp2",
+		"turret_mhp1",    // mhp = max health points
+		"turret_mhp2",
+		"turret_mhp3",
+		"turret_regen1",
+		"turret_regen2",
+		"turret_dmg_red1",     // damage reduction
+		"turret_dmg_red2",
+		"turret_dmg_red3",
+		"turret_dmg_red4",
+		"turret_lfsteal1",
+		"turret_lfsteal2",
+		"turret_lfsteal3",
+		"turret_range1",
+		"turret_range2",
+		"turret_range3",
+		"turret_range4",
+		"turret_ultimate",
+
+		"dome_init",
+		"dome_g_regen1",
+		"dome_g_regen2",
+		"dome_g_regen3",
+		"dome_g_hp1",
+		"dome_g_hp2",
+		"dome_g_hp3",
+		"dome_mhp1",
+		"dome_mhp2",
+		"dome_mhp3",
+		"dome_g_dmg_red1",  // global damage reduction
+		"dome_g_dmg_red2",
+		"dome_g_dmg_red3",
+		"dome_g_dmg1",
+		"dome_g_dmg2",
+		"dome_g_dmg3",
+		"dome_g_ri1",     // ri = research income
+		"dome_g_ri2",
+		"dome_g_ri3",
+		"dome_g_gi1",     // gi = gold income
+		"dome_g_gi2",
+		"dome_g_gi3",
+		"dome_ultimate",
+
+		"research_init",
+		"research_inc1",   // inc = income
+		"research_inc2",
+		"research_inc3",
+		"research_mhp1",
+		"research_mhp2",
+		"research_mhp3",
+		"research_regen1",
+		"research_regen2",
+
+		"gold_init",
+		"gold_inc1",
+		"gold_inc2",
+		"gold_inc3",
+		"gold_mhp1",
+		"gold_mhp2",
+		"gold_mhp3",
+		"gold_regen1",
+		"gold_regen2"
     };
 
     /*
@@ -212,7 +338,6 @@ public:
 		}
     }
 
-
 	bool unlockResearch(ResearchList research_name) {
 		if (resource_manager.spendResearch(research_list[research_name]->cost)) {
 			research_list[research_name]->unlocked = true;
@@ -266,6 +391,116 @@ public:
 
 	Point getLeftUpGraphCorner() {
 		return left_up_graph_corner;
+	}
+
+// start of in-game coefficients getters
+
+	/// turret researches block
+
+	float getTurretMaxHealthCCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getTurretDamageCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getTurretAttackSpeedCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getTurretRegenerationCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getTurretDamageReductionCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	float getTurretAttackRangeCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getTurretLifestealCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	// dome researches block
+
+	float getDomeGlobalRegenCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	float getDomeMaxHealthCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getDomeGlobalMaxHealthCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	float getDomeGlobalDamageReductionCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	float getDomeGlobalDamageBonusCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	float getDomeGlobalGoldIncomeCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	float getDomeGlobalResearchIncomeCoef() {
+		float coef = 0;
+		return coef;
+	}
+
+	// science base researches block
+
+	float getScienceIncomeCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getScienceMaxHealthCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getScienceRegenCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	// gold base researches block
+
+	float getGoldIncomeCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getGoldMaxHealthCoef() {
+		float coef = 1;
+		return coef;
+	}
+
+	float getGoldRegenCoef() {
+		float coef = 1;
+		return coef;
 	}
 
 } research_manager;
